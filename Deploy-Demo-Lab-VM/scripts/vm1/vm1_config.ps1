f2adda7e824e9ae1b97281b1b9dd704c9c99d572
Anthony Mashford <anthony@mashfords.com>
2021-07-30 23:29:19 +0100

    Update vm1_config.ps1

--- Deploy-Demo-Lab-VM/scripts/vm1/vm1_config.ps1
+++ Deploy-Demo-Lab-VM/scripts/vm1/vm1_config.ps1
@@ -25,3 +25,16 @@ Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv
 Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
 Install-windowsfeature -name DNS -IncludeManagementTools
 Clear-DnsClientCache
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "F:\windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "demo.lab" `
+-DomainNetbiosName "demo" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "F:\windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "F:\windows\SYSVOL" `
+-Force:$true
