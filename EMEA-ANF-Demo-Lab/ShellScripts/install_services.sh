1da89f8898b18631e2ad62ac1f0e2b3b0a767d07
Anthony Mashford <anthony@mashfords.com>
2021-08-20 17:33:49 +0100

    Update install_services.sh

--- EMEA-ANF-Demo-Lab/ShellScripts/install_services.sh
+++ EMEA-ANF-Demo-Lab/ShellScripts/install_services.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt-get update                # Get the latest package updates
 apt-get install nfs-common -y # Install NFS service
-apt-get install samba -y      # Install Samba service
+apt-get install samba -y      # Install Samba serviceterraform -v
